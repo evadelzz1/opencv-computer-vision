10645aa3378299c1cdb6ffe8628b80fadbd1dafe
evadelzz1 <evadel.zz1@gmail.com>
2024-01-23 22:53:45 +0900

    add securityCamera

---
+++ 10_application/130.SecurityCamera.py
@@ -0,0 +1,31 @@
+import cv2
+import time
+import datetime
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fullbody.xml")
+
+while True:
+    _, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, width, height) in faces:
+       cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3)
+
+    cv2.imshow("Camera", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:   # ESC key
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
+
+# https://www.youtube.com/watch?v=Exic9E5rNok
